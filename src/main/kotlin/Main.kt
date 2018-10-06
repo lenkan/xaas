2f15bc1da1f1139bfb666466cc09feadea973313
lenkan <dlenksjo@gmail.com>
2018-10-07 01:39:35 +0200

    Cleaning up

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -8,30 +8,64 @@ import java.io.File
 import javax.xml.transform.stream.StreamSource
 
 private val processor: Processor = Processor(false)
+private val hostname: String = System.getenv("AMQP_HOST") ?: "localhost"
+private val username: String = System.getenv("AMQP_USER") ?: "guest"
+private val password: String = System.getenv("AMQP_PASSWORD") ?: "guest"
+private val virtualHost: String = System.getenv("AMQP_VHOST") ?: "/"
+private val port: Int = (System.getenv("AMQP_PORT") ?: "5672").toInt()
+private val queueName: String = System.getenv("AMQP_QUEUE") ?: "xaas.transform"
+private val durable: Boolean = System.getenv("AMQP_DURABLE").parseBoolean(false)
+private val exclusive: Boolean = System.getenv("AMQP_EXCLUSIVE").parseBoolean(false)
+private val autoDelete: Boolean = System.getenv("AMQP_AUTODELETE").parseBoolean(true)
+private val root: String = System.getenv("XSLT_ROOT") ?: System.getProperty("user.home")+"/.xaas"
 
-fun stylesheets(): Map<String, XsltExecutable> {
-    val compiler = processor.newXsltCompiler()
-    val root = "/home/lenkan/code/tt/tnt/tools/xslt/"
-    return File(root).walkTopDown().filter { file ->
-        file.extension == "xsl"
-    }.map { file ->
-        val key = file.path.replace(root, "").replace(".xsl", "")
-        println("Compiling $key")
-        val source = StreamSource(file.bufferedReader())
-        val executable = compiler.compile(source)
-        key to executable
-    }.toMap()
+fun String?.parseBoolean(default: Boolean): Boolean = when (this) {
+    null -> default
+    "0" -> false
+    "false" -> false
+    else -> true
 }
 
-private fun transform(sheet: XsltExecutable, body: ByteArray): ByteArray {
-    val transformer = sheet.load()
-    val outputStream = ByteArrayOutputStream()
-
-    transformer.setSource(StreamSource(body.inputStream()))
-    transformer.destination = processor.newSerializer(outputStream)
-    transformer.transform()
-    return outputStream.toByteArray()
+fun sheetId(path: String): String {
+    return path.replace(root, "")
+            .replace(".xsl", "")
+            .replace(Regex("^/"), "")
 }
+
+
+class Transform(private val executable: XsltExecutable) {
+    fun exec(body: ByteArray): ByteArray {
+        val transformer = executable.load()
+        val outputStream = ByteArrayOutputStream()
+
+        transformer.setSource(StreamSource(body.inputStream()))
+        transformer.destination = processor.newSerializer(outputStream)
+        transformer.transform()
+        return outputStream.toByteArray()
+    }
+}
+
+class Transforms {
+    private var transforms: Map<String, Transform> = emptyMap()
+    private val compiler = processor.newXsltCompiler()
+
+    fun load() {
+        transforms = File(root).walkTopDown().filter { file ->
+            file.extension == "xsl"
+        }.map { file ->
+            val key = sheetId(file.path)
+            println("Compiling $key")
+            val source = StreamSource(file.bufferedReader())
+            val executable = compiler.compile(source)
+            key to Transform(executable)
+        }.toMap()
+    }
+
+    fun get(id: String): Transform? {
+        return transforms.getValue(id)
+    }
+}
+
 
 class Reply(private val channel: Channel, private val replyTo: String, private val correlationId: String) {
     fun badRequest(msg: String, status: Int = 400) {
@@ -67,9 +101,7 @@ class Reply(private val channel: Channel, private val replyTo: String, private v
     }
 }
 
-class TransformConsumer(channel: Channel, sheets: Map<String, XsltExecutable>) : DefaultConsumer(channel) {
-    private val sheets: Map<String, XsltExecutable> = sheets
-
+class TransformConsumer(channel: Channel, private val transforms: Transforms) : DefaultConsumer(channel) {
     override fun handleDelivery(consumerTag: String?, envelope: Envelope?, properties: AMQP.BasicProperties?, body: ByteArray?) {
         super.handleDelivery(consumerTag, envelope, properties, body)
         if (properties == null || body === null) {
@@ -84,34 +116,35 @@ class TransformConsumer(channel: Channel, sheets: Map<String, XsltExecutable>) :
         }
 
         val id = properties.headers.getValue("id").toString()
-        if (!sheets.containsKey((id))) {
-            return reply.badRequest("No such transform '$id'", 404)
-        }
-
-        var sheet = sheets.getValue(id)
-        val result = transform(sheet, body)
+        val transform = transforms.get(id) ?: return reply.badRequest("No such transform '$id'", 404)
+        val result = transform.exec(body)
         return reply.ok(result)
     }
 }
 
 
 fun main(args: Array<String>) {
-    val sheets = stylesheets()
+    val transforms = Transforms()
+    transforms.load()
 
     val factory = ConnectionFactory()
-    factory.username = "local"
-    factory.password = "panda4ever"
+    factory.username = username
+    factory.host = hostname
+    factory.password = password
+    factory.port = port
+    factory.virtualHost = virtualHost
     val connection = factory.newConnection()
     val channel = connection.createChannel()
-    val consumer = TransformConsumer(channel, sheets)
+    println("Connected to $hostname$port$virtualHost as $username")
+    println()
 
-    channel.queueDeclare(
-            "xslt.kt",
-            false,
-            false,
-            true,
-            null
-    )
 
-    channel.basicConsume("xslt.kt", consumer)
+    val consumer = TransformConsumer(channel, transforms)
+    channel.queueDeclare(queueName, durable, exclusive, autoDelete, null)
+    channel.basicConsume(queueName, consumer)
+
+    println("Listening on $queueName with settings:")
+    println("- durable: $durable")
+    println("- exclusive: $exclusive")
+    println("- auto-delete: $autoDelete")
 }
